3f37e4c4b0b12a6d14769fbb58605f2c8e38f593
DongJun Park <121371412+qkrehdwns032@users.noreply.github.com>
2025-08-10 11:14:28 +0900

    set

---
+++ sambakja/src/main/java/team3/sambakja/SambakjaApplication.java
@@ -0,0 +1,13 @@
+package team3.sambakja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SambakjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SambakjaApplication.class, args);
+	}
+
+}
