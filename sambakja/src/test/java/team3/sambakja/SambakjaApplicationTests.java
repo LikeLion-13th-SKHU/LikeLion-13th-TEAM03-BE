e0259fc277e105989074e54a8c92ee1bb8651bf8
Park, seun <seunpark04@gmail.com>
2025-08-14 19:05:55 +0900

    Initial Setting

---
+++ sambakja/src/test/java/team3/sambakja/SambakjaApplicationTests.java
@@ -0,0 +1,13 @@
+package team3.sambakja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SambakjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
