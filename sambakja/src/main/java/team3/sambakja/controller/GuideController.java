c16a214495dfde9b8b3ae143d0dca6b4cf530720
Park, seun <seunpark04@gmail.com>
2025-08-15 18:32:24 +0900

    [fix] @RestController에 직접 경로 지정한 부분을 @RequestMapping으로 분리

--- sambakja/src/main/java/team3/sambakja/controller/GuideController.java
+++ sambakja/src/main/java/team3/sambakja/controller/GuideController.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team3.sambakja.service.GuideService;
 
-@RestController("/api/guide")
+@RestController
+@RequestMapping("/api/guide")
 @RequiredArgsConstructor
 public class GuideController {
 
