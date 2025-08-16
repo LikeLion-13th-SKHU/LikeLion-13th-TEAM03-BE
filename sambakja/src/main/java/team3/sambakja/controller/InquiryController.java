fb526d3faa30c1b4579ba19efd17c832b260cf03
Park, seun <seunpark04@gmail.com>
2025-08-16 16:18:23 +0900

    [decs](#8) Swagger 문서 추가

--- sambakja/src/main/java/team3/sambakja/controller/InquiryController.java
+++ sambakja/src/main/java/team3/sambakja/controller/InquiryController.java
@@ -1,5 +1,8 @@
 package team3.sambakja.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +18,14 @@ public class InquiryController {
 
     private final InquiryService inquiryService;
 
+    @Operation(
+            summary = "문의 이메일 전송",
+            description = "사용자의 이름, 연락처, 이메일, 문의 내용을 입력 받아 담당자 이메일로 전송합니다."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "문의 전송 성공"),
+            @ApiResponse(responseCode = "500", description = "이메일 전송 실패")
+    })
     @PostMapping
     public void submitInquiry(@RequestBody InquiryRequest request) {
         inquiryService.sendInquiryEmail(request);
