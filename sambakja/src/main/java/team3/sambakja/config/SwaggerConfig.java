e70db2470e6c339d629475953debe833d9acae20
DongJun Park <121371412+qkrehdwns032@users.noreply.github.com>
2025-08-18 15:12:48 +0900

    fix swagger URL

--- sambakja/src/main/java/team3/sambakja/config/SwaggerConfig.java
+++ sambakja/src/main/java/team3/sambakja/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
                 .description("서울 소상공인 맞춤 지역·업종 데이터 기반 종합 분석 리포트 AI 에이전트 '어디가게?' API 명세서");
 
         return new OpenAPI()
-                .addServersItem(new Server().url("http://3.35.124.89:8082"))
+                .addServersItem(new Server().url("https://3.35.124.89:8082"))
                 .info(info);
     }
 
